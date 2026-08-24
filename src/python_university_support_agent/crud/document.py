4e6cb754ec75f44aa5b074ec870a2b349c8ac216
demonize-c <sourabmajhi.200@gmail.com>
2026-08-24 14:52:03 +0530

    fix: update init_db script and fix database session and models

--- src/python_university_support_agent/crud/document.py
+++ src/python_university_support_agent/crud/document.py
@@ -1,16 +1,24 @@
 from sqlalchemy.orm import Session
 from ..schemas import DocumentCreate
-from ..models  import Document
- 
-def create_document(db: Session, doc: DocumentCreate):
-   doc = Document(
-      title    = DocumentCreate.title,
-      content  = DocumentCreate.content,
-      filename = DocumentCreate.filename,
-      metadata = DocumentCreate.metadata,
-      emebedded= DocumentCreate.embedded
-   );
+from ..models import Document
 
-   db.add( doc )
-   db.commit()
-   db.flush()
+
+def create_document(db: Session, doc_in: DocumentCreate) -> Document:
+    ext = doc_in.extension
+    if not ext and "." in doc_in.filename:
+        ext = doc_in.filename.rsplit(".", 1)[-1]
+
+    db_doc = Document(
+        title=doc_in.title,
+        content=doc_in.content,
+        filename=doc_in.filename,
+        extension=ext,
+        doc_metadata=doc_in.metadata,
+        embedded=doc_in.embedded,
+    )
+
+    db.add(db_doc)
+    db.commit()
+    db.refresh(db_doc)
+    return db_doc
+
